50e8bb9d32d263cc1e2aceca63c266beac8b1c29
SparshKapoor-CODER <sparsh312333@gmail.com>
2026-02-07 19:44:58 +0530

    Add Play.java to implement a solution for the problem

---
+++ Div 2/1007/Play.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Play {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int k = sc.nextInt();
+
+            if (k % 3 == 1) {
+                System.out.println("YES");
+            }
+            
+            else {
+                System.out.println("NO");
+            }
+        }
+    }
+}
